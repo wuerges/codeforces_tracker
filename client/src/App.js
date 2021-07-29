b224b0c1e617d4b87a24837c5f1e9e00987eb5e9
Emilio Wuerges <wuerges@gmail.com>
2021-07-29 10:13:16 -0300

    created client, need to config cors

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,61 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+
+
 
 function App() {
+  
+  const [contests, setContests] = useState(0);
+  const [official, setOfficial] = useState(0);
+  const [user, setUser] = useState(0);
+  
+  const countOfficial=()=> {
+    fetch(`https://codeforces.com/api/user.rating?handle=${user}`
+    ,{
+      headers : { 
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+       }
+
+    })
+    .then((response) => {
+      return response.json();
+    })
+    .then((myJson) => {
+      setOfficial(myJson.result.length);
+    });
+
+  }
+  const getData=()=>{
+    fetch('fake.json'
+    ,{
+      headers : { 
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+       }
+    }
+    )
+      .then(function(response){
+        // console.log(response)
+        return response.json();
+      })
+      .then(function(myJson) {
+        setContests(myJson.result.count);
+        setUser(myJson.result.user);
+        // console.log(myJson);
+      });
+  }
+  useEffect(()=>{
+    countOfficial();
+    getData();
+  },[]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p> {user} </p>
+      <p> {contests} </p>      
+      <p> {official} </p>      
     </div>
   );
 }
